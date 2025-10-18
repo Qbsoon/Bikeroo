8148ccafa6ff5aad57a1e4f8474df0a80142656a
Jakub Grula <ramsters110@gmail.com>
2025-10-18 20:00:14 +0200

    Stworzenie formularzy

--- Bikeroo/main.Designer.cs
+++ Bikeroo/main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Bikeroo
 {
-    partial class Form1
+    partial class main
     {
         /// <summary>
         ///  Required designer variable.
--- Bikeroo/main.cs
+++ Bikeroo/main.cs
@@ -1,8 +1,8 @@
 namespace Bikeroo
 {
-    public partial class Form1 : Form
+    public partial class main : Form
     {
-        public Form1()
+        public main()
         {
             InitializeComponent();
             
